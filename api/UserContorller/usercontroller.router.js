0536f9fa5552524e739fa0f431a4196d24d8cbdc
Amal-anil <amalanilmkm@gmail.com>
2026-06-18 19:59:11 +0530

    token

--- api/UserContorller/usercontroller.router.js
+++ api/UserContorller/usercontroller.router.js
@@ -1,13 +1,14 @@
 const {
   loginUserDetail,
   RegisterUser,
-
+  logoutUserSession,
 } = require("./usercontroller.controller");
 
+const authMiddleware = require("../../Middleware/auth.middleware");
 const router = require("express").Router();
 
 router.post("/login", loginUserDetail);
 router.post("/signin", RegisterUser);
-
+router.post("/logout", authMiddleware, logoutUserSession);
 
 module.exports = router;
